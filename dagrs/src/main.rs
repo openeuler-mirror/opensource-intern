6330375fb94e7030c5b711c40a19dae6f369fdab
Yinwhe <wyffeiwhe@gmail.com>
2022-01-26 23:34:42 +0800

    Intern dagrs

--- dagrs/src/main.rs
+++ dagrs/src/main.rs
@@ -1,3 +1,25 @@
+/*
+ * @Author: Yinwhe
+ * @Date: 2022-01-19 14:14:28
+ * @LastEditors: Yinwhe
+ * @LastEditTime: 2022-01-26 23:39:46
+ * @Description: dagrs
+ * @Copyright: Copyright (c) 2022
+ */
+
+extern crate bimap;
+extern crate yaml_rust;
+
+mod error_handler;
+mod graph;
+mod task;
+mod dag_engine;
+
+use dag_engine::DagEngine;
+
 fn main() {
-    println!("Hello, world!");
+    let mut dagrs = DagEngine::new();
+    if let Err(e) = dagrs.run("test/test1.yaml") {
+        println!("[Error] {}", e);
+    }
 }
