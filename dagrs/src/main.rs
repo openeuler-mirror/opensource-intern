2e756567c8ca9940c0e4b68ee527e07371b9937e
Yinwhe <wyffeiwhe@gmail.com>
2022-02-06 13:43:30 +0800

    Intern dagrs

--- dagrs/src/main.rs
+++ dagrs/src/main.rs
@@ -2,24 +2,51 @@
  * @Author: Yinwhe
  * @Date: 2022-01-19 14:14:28
  * @LastEditors: Yinwhe
- * @LastEditTime: 2022-01-26 23:39:46
+ * @LastEditTime: 2022-02-06 13:48:41
  * @Description: dagrs
- * @Copyright: Copyright (c) 2022
  */
 
 extern crate bimap;
 extern crate yaml_rust;
 
+mod dag_engine;
 mod error_handler;
 mod graph;
 mod task;
-mod dag_engine;
 
 use dag_engine::DagEngine;
 
 fn main() {
     let mut dagrs = DagEngine::new();
-    if let Err(e) = dagrs.run("test/test1.yaml") {
-        println!("[Error] {}", e);
+    if let Some(filename) = std::env::args().nth(1) {
+        if let Err(e) = dagrs.run(&filename) {
+            println!("[Error] {}", e);
+        }
+    } else {
+        println!("Usage: dargs <filename>")
     }
+}
+
+#[test]
+fn test1() {
+    let res = DagEngine::new().run("test/test1.yaml").unwrap();
+    assert_eq!(res, false);
+}
+
+#[test]
+fn test2() {
+    let res = DagEngine::new().run("test/test2.yaml").unwrap();
+    assert_eq!(res, true)
+}
+
+#[test]
+fn test3() {
+    let res = DagEngine::new().run("test/test3.yaml").unwrap();
+    assert_eq!(res, false)
+}
+
+#[test]
+fn test4() {
+    let res = DagEngine::new().run("test/test4.yaml").unwrap();
+    assert_eq!(res, false)
 }
